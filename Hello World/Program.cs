211874ef391e9c328df730f33053e59b67d239fc
Lamark Neris Reis <lamark.reis@programmers.com.br>
2022-05-16 11:59:59 -0300

    Estudos C#

---
+++ Hello World/Program.cs
@@ -0,0 +1,47 @@
+﻿using Hello_World.Domain;
+
+
+//Calcular média de idade
+
+var idadeMinima = 20;
+
+var idadeMaxima = 50;
+
+var mediaAluno = (idadeMinima + idadeMaxima) / 2;
+
+Console.WriteLine(mediaAluno);
+
+
+
+// Conversor de moedas
+
+double euro, valorReal;
+
+euro = 5.26;
+
+Console.WriteLine("Insira o valor da compra:");
+
+valorReal = double.Parse(Console.ReadLine()) * euro;
+
+Console.WriteLine($"O valor da sua compra em reais é: {valorReal}");
+
+
+// Comissão de vendas
+
+double comissao, valorVenda;
+
+valorVenda = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira o valor vendido:");
+
+comissao = (valorVenda/ 20) + valorVenda;
+
+
+Console.WriteLine($"O valor da sua comissão é: {comissao}");
+
+
+//
+
+
+
+
